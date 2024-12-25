e8faeae7b8a21e22de5bb56dc660673865fcd07d
psp10 <psp10@SHAHAD>
2024-12-25 13:24:22 +0300

    Add project files.

---
+++ CodedNotionTasks25122024/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+using CodedNotionTasks25122024.models;
+
+UserNameClass user = new UserNameClass();
+
+Console.WriteLine("enter your user name ");
+user.UserName = Console.ReadLine(); //we dont write name because its private and wont show
+Console.WriteLine(user.UserName);  //we dont write name because its private and wont show
+user.CorrectUser();
+
+Console.WriteLine("enter your salary: ");
+user.salary =Convert.ToDouble( Console.ReadLine());
+Console.WriteLine(user.salary);
+
+
+
---
+++ CodedNotionTasks25122024/models/UserNameClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodedNotionTasks25122024.models
+{
+    internal class UserNameClass
+    {
+        private string name;
+        public string UserName
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+                else {
+                    Console.WriteLine("please enter user name: ");
+
+                   }
+            }
+        }
+        public void CorrectUser()
+        {
+            Console.WriteLine("your username is " + name);//int the else, we saved the value inside name from above, hence why we get it here when we call it in the main
+        }
+
+        private double Salary;
+        public double salary
+        {
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                if (value >= 350 && value <= 900)
+                {
+                    Salary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Salary is out of range!");
+                }
+            }
+        }
+    }
+}
